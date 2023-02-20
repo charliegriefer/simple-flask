959a53e8bfac2b34a33c824f1635dac6857c5dab
Charlie Griefer <charlie@griefer.com>
2023-02-19 21:01:04 -0700

    Cleanup

--- flaskapp/__init__.py
+++ flaskapp/__init__.py
@@ -1,25 +1,23 @@
-import json
 import os
 
+import requests
+from dotenv import load_dotenv
 from flask import Flask, render_template
 
-import requests
-
-from dotenv import load_dotenv
-
-load_dotenv(dotenv_path=os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'), verbose=True)
+load_dotenv(dotenv_path=os.path.join(os.path.dirname(os.path.dirname(__file__)), ".env"), verbose=True)
 
 app = Flask(__name__)
 
-app.config.from_mapping(SECRET_KEY=os.environ['SECRET_KEY'])
+app.config.from_mapping(SECRET_KEY=os.environ["SECRET_KEY"])
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    instance_id = 'Unknown'
+    instance_id = "Unknown"
     try:
-        response = requests.get('http://169.254.169.254/latest/meta-data/instance-id/', timeout=3)
-        instance_id = response.content.decode('utf-8')
+        response = requests.get("http://169.254.169.254/latest/meta-data/instance-id/", timeout=3)
+        instance_id = response.content.decode("utf-8")
     except:
         pass
 
-    return render_template('index.html', instance_id=instance_id)
+    return render_template("index.html", instance_id=instance_id)
